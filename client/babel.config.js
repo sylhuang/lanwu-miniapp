e356c05f1eb3bd9dc6d3f9f5c767c04e94f90cd0
Sylvia Huang <sylviahuang@ssw.com.au>
2023-05-08 12:47:42 +0800

    fix config

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,8 +1,10 @@
 // babel-preset-taro 更多选项和默认值：
 // https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
-export const presets = [
-  ['taro', {
-    framework: 'react',
-    ts: true
-  }]
-];
+module.exports = {
+  presets: [
+    ['taro', {
+      framework: 'react',
+      ts: true
+    }]
+  ]
+}
--- client/src/pages/index/index.tsx
+++ client/src/pages/index/index.tsx
@@ -3,7 +3,7 @@ import Taro, { Config } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
 import './index.less'
 
-import Login from '../../components/login/index'
+import Login from '../../components/login/index.weapp'
 
 export default class Index extends Component<PropsWithChildren> {
   componentDidMount () { }
